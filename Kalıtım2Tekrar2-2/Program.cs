4721dd770cf655445588b691e9a58a5686a2bd07
Mert <mertbasar0@hotmail.com>
2022-03-08 23:07:14 +0300

    asd

---
+++ Kalıtım2Tekrar2-2/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Four f = new Four(4444);
+public class One
+{
+    public int MyProperty { get; set; }
+    public One(int a)
+    {
+        Console.WriteLine(a);
+    }
+}
+
+public class Two :One
+{
+    public Two(int p) :base(p)
+    {
+
+    }
+}
+
+public class Tree : Two
+{
+    public Tree(int t) : base(t)
+    {
+
+    }
+}
+
+public class Four : Tree
+{
+    public Four(int k) : base (k) 
+    {
+
+    }
+}
